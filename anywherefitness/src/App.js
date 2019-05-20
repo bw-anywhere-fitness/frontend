056ac0bf8b002457c17686930bc31761d88626e3
tjr226 <tjr226@gmail.com>
2019-05-20 12:51:59 -0400

    First commit
    placeholders where Routes will go later

---
+++ anywherefitness/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login placeholder</h1>
+      <h2>Teachers Route placeholder</h2>
+      <h2>Students Route placeholder</h2>
+    </div>
+  );
+}
+
+export default App;
